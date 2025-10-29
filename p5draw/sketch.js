ce26676898d59c16632fc42e0b4a9f55bf36af39
Thiago Hersan <thiago.hersan+github@gmail.com>
2025-10-29 12:30:35 -0400

    add p5 example

---
+++ p5draw/sketch.js
@@ -0,0 +1,99 @@
+const jsonUrl =
+  "https://huggingface.co/datasets/visualizedata/revolutionary_silhouettes/resolve/main/json/revolutionary_silhouettes-original.json";
+
+const imgUrl =
+  "https://pgdv-5200-2025f-a.github.io/silhouettes/imgs/00_original";
+
+let silData = [];
+
+let img;
+let points = [];
+
+async function loadData() {
+  const dataText = await loadStrings(jsonUrl);
+
+  for (let i = 0; i < dataText.length - 1; i++) {
+    const rowData = JSON.parse(dataText[i]);
+    silData.push(rowData);
+  }
+}
+
+async function loadByFilename(filename) {
+  const found = silData.filter((x) => x.id == filename);
+  if (found.length > 0) {
+    const rowData = found[0];
+    img = await loadImage(`${imgUrl}/${rowData.id}.jpg`);
+    points = getSilhouette(rowData);
+  }
+}
+
+function getSilhouette(rowData) {
+  const mykeys = Object.keys(rowData);
+  const xs = [];
+  const ys = [];
+
+  for (let k of mykeys) {
+    if (k.startsWith("x")) {
+      xs.push(rowData[k]);
+    }
+    if (k.startsWith("y")) {
+      ys.push(rowData[k]);
+    }
+  }
+
+  const points = [];
+  for (let idx = 0; idx < xs.length; idx++) {
+    points.push({
+      x: xs[idx],
+      y: ys[idx],
+    });
+  }
+  return points;
+}
+
+let pidx = 0;
+let sFactor = 1;
+
+function setImageSources() {
+  Array.from(document.getElementsByClassName("original-image")).forEach(
+    (el) => {
+      el.src = `${imgUrl}/${el.dataset.filename}.jpg`;
+
+      el.addEventListener("click", async (evt) => {
+        const imgBB = evt.target.getBoundingClientRect();
+        clear();
+        resizeCanvas(imgBB.width, imgBB.height);
+
+        await loadByFilename(evt.target.dataset.filename);
+
+        const canvasContainerEl = document.getElementById("canvas-container");
+        canvasContainerEl.style.top = `${imgBB.top}px`;
+        canvasContainerEl.style.left = `${imgBB.left}px`;
+
+        sFactor = imgBB.height / img.height;
+        pidx = 0;
+        clear();
+      });
+    }
+  );
+}
+
+async function setup() {
+  let mCanvas = createCanvas(0, 0);
+  mCanvas.parent("canvas-container");
+
+  setImageSources();
+  await loadData();
+}
+
+function draw() {
+  fill(255, 0, 0);
+  noStroke();
+
+  for (let i = 0; i < pidx; i++) {
+    const p = points[i];
+    ellipse(p.x * sFactor, p.y * sFactor, 5, 5);
+  }
+
+  pidx = min(pidx + 4, points.length);
+}
